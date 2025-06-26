43cec0b6db7cd5a3e3e6290a686f3da613b4135f
lulostupnik <lulostupnik2003@gmail.com>
2025-06-26 17:12:32 -0300

    request.c

--- src/Request/request.c
+++ src/Request/request.c
@@ -4,7 +4,6 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include "requestParser.h"
-#include "helper.h"
 
 void requestReadInit(unsigned state, struct selector_key *key) {
     printf("Inicio lectura de request\n");
