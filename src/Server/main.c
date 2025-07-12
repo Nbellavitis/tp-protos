ec0e9d1d9f3c3bfbe113ba8ec838671d044f188a
nbellavitis <nbellavitisalzate@itba.edu.ar>
2025-07-12 13:46:32 -0300

    auth default

--- src/Server/main.c
+++ src/Server/main.c
@@ -195,7 +195,7 @@ int main (int argc,char * argv[]){
     signal(SIGTERM, sig_handler);
     signal(SIGINT, sig_handler);
     selector_status ss= SELECTOR_SUCCESS;
-    setAuthMethod(NOAUTH); // Establecer el método de autenticación por defecto
+    setAuthMethod(AUTH); // Establecer el método de autenticación por defecto
     char * error = NULL;
     struct selector_init conf = {
         .signal = SIGALRM,
